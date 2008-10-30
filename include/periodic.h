bea464dbc752710ff29d987d4fad4295b58569c7
David Shaw <dshaw@jabberwocky.com>
2008-10-30 22:29:09 +0000

    timewarp callback

--- include/periodic.h
+++ include/periodic.h
@@ -8,9 +8,10 @@
 #define PERIODIC_DELAY 1
 
 struct periodic_t *periodic_add(unsigned int interval,unsigned int flags,
-				void (*routine)(time_t,void *),void *arg);
+				void (*callback)(time_t,void *),void *arg);
 int periodic_start(unsigned int concurrency,unsigned int flags);
 int periodic_stop(void);
-int periodic_timewarp(unsigned int interval,unsigned int warptime);
+int periodic_timewarp(unsigned int interval,unsigned int warptime,
+		      void (*callback)(void *),void *arg);
 
 #endif /* !_PERIODIC_H_ */
